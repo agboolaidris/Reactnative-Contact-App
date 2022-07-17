a919b013bf463d4884318d6b068d8c770e1468a8
Iris <agboolaisholaidreez>
2022-07-17 08:27:56 -0700

    update the navigation setup

--- src/navigation/stack.tsx
+++ src/navigation/stack.tsx
@@ -1,21 +1,20 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "../screens/home";
 import Login from "../screens/login";
 import Register from "../screens/register";
 
 const Stack = createNativeStackNavigator();
 
-export const PrivateStack = () => {
-  return (
-    <>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Notifications" component={HomeScreen} />
-      </Stack.Navigator>
-    </>
-  );
-};
+// export const PrivateStack = () => {
+//   return (
+//     <>
+//       <Stack.Navigator initialRouteName="Home">
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Notifications" component={HomeScreen} />
+//       </Stack.Navigator>
+//     </>
+//   );
+// };
 
 export const PublicStack = () => {
   return (
