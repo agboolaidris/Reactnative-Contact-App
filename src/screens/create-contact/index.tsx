c0d8ee5294e48ff27ba73e66aed2ac49b9e36960
Iris <agboolaisholaidreez>
2022-07-17 08:21:24 -0700

    update

---
+++ src/screens/create-contact/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CreateContact = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>CreateContact</Text>
+    </View>
+  );
+};
+
+export default CreateContact;
